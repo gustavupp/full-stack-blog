82fa80f9b8b3c39143f58c2aebe8e31e36733df2
Gustavo Pereira <xgustavux@hotmail.com>
2022-01-24 23:22:50 +1000

    basic bootstrap interface done

--- client/react-mysql-blog/src/App.js
+++ client/react-mysql-blog/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [post, setPost] = useState([])
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="container">
+      <h1 className="my-4 text-center">Blog Posts</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label for="title">Post Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            aria-describedby="title"
+            placeholder="Enter title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div class="form-group">
+          <label for="textarea">Post Content</label>
+          <textarea
+            class="form-control"
+            id="textarea"
+            rows="7"
+            placeholder="Your blog post..."
+            onChange={(e) => setContent(e.target.value)}
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </main>
+  )
 }
 
-export default App;
+export default App
