225a2674a9a3b439be1ecacab7a8d1a1c9503c0f
Gustavo Pereira <xgustavux@hotmail.com>
2022-02-06 09:56:55 +1000

    changed db connection config

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //express
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
 const cors = require('cors')
 require('dotenv').config()
 
@@ -8,76 +9,79 @@ app.use(cors())
 app.use(express.json())
 
 /****************************mysql connection*********************************/
-const mysql = require('mysql')
-const db = mysql.createConnection({
+const db = mysql.createPool({
+  connectionLimit: 100,
   user: 'b3d2af5763bd4b',
   host: 'us-cdbr-east-05.cleardb.net',
   password: '1b7b746f',
   database: 'heroku_74fdc48ba3d5fda',
 })
-mysql: db.connect()
 /****************************mysql connection*********************************/
 
-//post request endpoint
-app.post('/post', (req, res) => {
-  const name = req.body.name
-  const email = req.body.email
-  const gender = req.body.gender
-  const status = req.body.status
+db.getConnection((err, connection) => {
+  if (err) throw err
+  console.log('connected as id ' + connection.threadId)
+  //post request endpoint
+  app.post('/post', (req, res) => {
+    const name = req.body.name
+    const email = req.body.email
+    const gender = req.body.gender
+    const status = req.body.status
 
-  db.query(
-    'INSERT INTO users (userName, email, gender, userStatus) VALUES (?, ?,?,?)',
-    [name, email, gender, status],
-    (err, result) => {
-      if (err) console.log(err)
-      else {
-        res.send('values inserted!')
+    db.query(
+      'INSERT INTO users (userName, email, gender, userStatus) VALUES (?, ?,?,?)',
+      [name, email, gender, status],
+      (err, result) => {
+        if (err) console.log(err)
+        else {
+          res.send('values inserted!')
+        }
       }
-    }
-  )
-})
-
-//get request endpoint
-app.get('/get', (req, res) => {
-  db.query('SELECT * FROM users;', (err, result) => {
-    if (err) console.log(err)
-    else res.send(result)
+    )
   })
-})
 
-//delete request endpoint
-app.delete('/delete/:id', (req, res) => {
-  const id = req.params.id
-  db.query('DELETE FROM users WHERE id = ?', id, (err, result) => {
-    if (err) console.log(err)
-    else res.send(result)
-  })
-})
-
-//update endpoint
-app.put('/user', (req, res) => {
-  const id = req.body.id
-  const name = req.body.name
-  const email = req.body.email
-  const gender = req.body.gender
-  const status = req.body.status
-
-  db.query(
-    'UPDATE users SET userName = ?, email = ?, gender = ?, userStatus = ? WHERE id = ?',
-    [name, email, gender, status, id],
-    (err, result) => {
+  //get request endpoint
+  app.get('/get', (req, res) => {
+    db.query('SELECT * FROM users;', (err, result) => {
       if (err) console.log(err)
       else res.send(result)
-    }
-  )
-})
+    })
+  })
 
-app.get('/', (req, res) => {
-  res.send('server running')
+  //delete request endpoint
+  app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id
+    db.query('DELETE FROM users WHERE id = ?', id, (err, result) => {
+      if (err) console.log(err)
+      else res.send(result)
+    })
+  })
+
+  //update endpoint
+  app.put('/user', (req, res) => {
+    const id = req.body.id
+    const name = req.body.name
+    const email = req.body.email
+    const gender = req.body.gender
+    const status = req.body.status
+
+    db.query(
+      'UPDATE users SET userName = ?, email = ?, gender = ?, userStatus = ? WHERE id = ?',
+      [name, email, gender, status, id],
+      (err, result) => {
+        if (err) console.log(err)
+        else res.send(result)
+      }
+    )
+  })
 })
 
 //initiate server and start listening for requests on PORT
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('server running')
 })
